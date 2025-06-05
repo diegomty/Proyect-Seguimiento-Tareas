0cbb704e3bf5048eb13e33e5885010043c41fd0e
diegomty <jperez256@alumnos.uaq.mx>
2025-06-05 15:16:18 -0600

    modificacion en importacion de librerias

--- frontend/app-tareas/src/components/ObjetivoForm.jsx
+++ frontend/app-tareas/src/components/ObjetivoForm.jsx
@@ -1,4 +1,3 @@
-// src/components/ObjetivoForm.jsx
 import React, { useState, useEffect } from 'react';
 
 function ObjetivoForm({ onSubmit, initialData = null, isEditMode = false }) {
--- frontend/app-tareas/src/pages/ObjetivoDetailPage.jsx
+++ frontend/app-tareas/src/pages/ObjetivoDetailPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/ObjetivoDetailPage.jsx
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import {
